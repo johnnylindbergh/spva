5c5b16c7b5b7d19548057ad2c7fde5329620f12b
Johnny Lindbergh <lindberghjohnny@gmail.com>
2024-11-18 23:15:27 +0000

    various changes

--- settings.js
+++ settings.js
@@ -9,20 +9,20 @@ module.exports = {
   PORT: 8080,
 
   // is the system in development mode (explicit error messages, etc)
-  DEV_MODE: true,
+  DEV_MODE: 0,
 
   // name of database
   DB_NAME: 'spvaTrack',
 
   // domain through which server is accessible
-  DOMAIN: 'https://localhost',
+  DOMAIN: 'https://estimate.sunpaintingva.com',
 
   /*  does the system allow automatic creation of new user accounts
       when authentication is attempted. */
   ALLOW_NEW_ACCOUNTS: true,
 
   // project path used for uploads. No tailing slash
-  PROJECT_PATH: '/Users/johnlindbergh/Documents/spva', 
+  PROJECT_PATH: '/var/www/spvaTrack', 
 
   /*  regex restriction to apply to emails of new accounts requesting access 
       (only if automatic creation enabled) */
@@ -30,7 +30,7 @@ module.exports = {
 
   // name of this system
   SYSTEM_NAME: 'SPVATrack',
-
+  // PROMPT SHOULD BE STORED IN THE DB
   PROMPT:"Consider the following json object. The output must be two description sections titled 'Proposal Includes' and 'Exclusions and assumptions' separated by a </br> tag. If an object has no selected materials, its name is listed in the 'Exclusions and assumptions' section; otherwise, a one-sentence description in the 'Proposal Includes' section that includes the name. \n. Do not include extra symbols like (* or -)",
  
 
