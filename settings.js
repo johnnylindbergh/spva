0286f347737dd3ffa7e4377bde9b3c295726f1d1
anjali8203 <anjali8203@gmail.com>
2024-12-12 10:38:36 -0500

    added varient to modify chatgpt prompt and to adjust numbers

--- settings.js
+++ settings.js
@@ -23,7 +23,7 @@ module.exports = {
   ALLOW_NEW_ACCOUNTS: true,
 
   // project path used for uploads. No tailing slash
-  PROJECT_PATH: '/var/www/spvaTrack', 
+  PROJECT_PATH: '/var/www/spva', 
 
   /*  regex restriction to apply to emails of new accounts requesting access 
       (only if automatic creation enabled) */
